fb05b74174d0094d25be22cf13d5ca25c43f4f4a
hashylog <108955419+hashylog@users.noreply.github.com>
2025-03-21 18:17:17 -0300

    general changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
+
+  slidervalue = 1;
+  public letters = false;
+  public uppercase = false;
+  public lowercase = false;
+  public numbers = false;
+  public symbols = false;
+
+  passwordvalue = "";
+  lowercasechars = "abcdefghijklmnopqrstuvwxyz";
+  uppercasechars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  numberschars = "1234567890";
+  symbolschar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
   
+  GeneratePassword() {
+    
+    let password = "";
+    let characters = "";
+    
+    if (this.uppercase) { characters += this.uppercasechars; }
+    if (this.lowercase) { characters += this.lowercasechars; }
+    if (this.numbers) { characters += this.numberschars; }
+    if (this.symbols) { characters += this.symbolschar; }
+
+    if (characters.length <= 0) {
+      characters += this.uppercasechars;
+      characters += this.lowercasechars;
+    }
+    
+
+    for (let i = 0; i < this.slidervalue; i++) { password += characters[Math.floor(Math.random() * characters.length)]; }
+
+    this.passwordvalue = password;
+
+  }
+
+  UpdateValue(event: Event)
+  {
+    const inputelement = event.target as HTMLInputElement;
+    this.slidervalue = inputelement.valueAsNumber;
+  }
 }
